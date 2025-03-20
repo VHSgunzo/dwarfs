1a655f4458b71f6dc70b8afab3f48b641cd46c13
VHSgunzo <wosganzo@gmail.com>
2025-03-20 23:01:11 +0300

    Update CI

--- build.sh
+++ build.sh
@@ -3,24 +3,23 @@ set -e
 
 DIR_SCRIPT="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-BUILD_ARCH="${BUILD_ARCH:=$1}"
-BUILD_ARCH="${BUILD_ARCH:=amd64}"
+for BUILD_ARCH in arm64 amd64
+  do
+    # Create build environment
+    cd "$DIR_SCRIPT"/.docker
 
-# Create build environment
-cd "$DIR_SCRIPT"/.docker
+    docker build . -t dwarfs --platform linux/$BUILD_ARCH -f ./Dockerfile.ubuntu \
+      --build-arg ARCH="$BUILD_ARCH" \
+      --build-arg SCRIPT=build-linux.sh
 
-docker build . -t dwarfs:"$BUILD_ARCH" -f ./Dockerfile.ubuntu \
-  --build-arg ARCH="$BUILD_ARCH" \
-  --build-arg SCRIPT=build-linux.sh
-
-# Build dwarfs
-cd "$DIR_SCRIPT"
-mkdir -p dist
-docker run --rm --user 0:0 \
-  --mount type=bind,source="$PWD",target=/workspace \
-  --env BUILD_TYPE=clang-release-notest-ninja-static-build \
-  --env BUILD_ARCH="$BUILD_ARCH" \
-  --env BUILD_DIST=ubuntu \
-  --env CFLAGS='-Os -g0 -ffunction-sections -fdata-sections -fvisibility=hidden -fmerge-all-constants' \
-  --env LDFLAGS='-Wl,--gc-sections -Wl,--strip-all' \
-  dwarfs:"$BUILD_ARCH"
+    # Build dwarfs
+    cd "$DIR_SCRIPT"
+    docker run --rm --user 0:0 \
+      --mount type=bind,source="$PWD",target=/workspace \
+      --env BUILD_TYPE=clang-release-notest-ninja-static-build \
+      --env BUILD_ARCH="$BUILD_ARCH" \
+      --env BUILD_DIST=ubuntu \
+      --env CFLAGS='-Os -g0 -ffunction-sections -fdata-sections -fvisibility=hidden -fmerge-all-constants' \
+      --env LDFLAGS='-Wl,--gc-sections -Wl,--strip-all' \
+      dwarfs
+done
