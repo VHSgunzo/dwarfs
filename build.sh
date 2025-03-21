f7fd9eaa6f71fdede48ab52bbaa742dcfd28b9e2
VHSgunzo <wosganzo@gmail.com>
2025-03-21 10:10:29 +0300

    Update CI

--- build.sh
+++ build.sh
@@ -3,24 +3,23 @@ set -e
 
 DIR_SCRIPT="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-for BUILD_ARCH in arm64 amd64
-  do
-    # Create build environment
-    cd "$DIR_SCRIPT"/.docker
+BUILD_ARCH="${BUILD_ARCH:=$1}"
+BUILD_ARCH="${BUILD_ARCH:=amd64}"
 
-    docker build . -t dwarfs --platform linux/$BUILD_ARCH -f ./Dockerfile.ubuntu \
-      --build-arg ARCH="$BUILD_ARCH" \
-      --build-arg SCRIPT=build-linux.sh
+# Create build environment
+#cd "$DIR_SCRIPT"/.docker
 
-    # Build dwarfs
-    cd "$DIR_SCRIPT"
-    docker run --rm \
-      --mount type=bind,source="$PWD",target=/workspace \
-      --env BUILD_TYPE=clang-release-notest-ninja-static-build \
-      --env BUILD_ARCH="$BUILD_ARCH" \
-      --env BUILD_DIST=ubuntu \
-      --env CFLAGS='-Os -g0 -ffunction-sections -fdata-sections -fvisibility=hidden -fmerge-all-constants' \
-      --env LDFLAGS='-Wl,--gc-sections -Wl,--strip-all' \
-      --env MAKEFLAGS="-j$(nproc)" \
-      dwarfs
-done
+#docker build . -t vhsgunzo/dwarfs-universal-build-image:latest \
+#  --platform linux/amd64,linux/arm64 -f ./Dockerfile.ubuntu
+
+# Build dwarfs
+cd "$DIR_SCRIPT"
+docker run --rm --platform linux/$BUILD_ARCH \
+  --mount type=bind,source="$PWD",target=/workspace \
+  --env BUILD_TYPE=clang-release-notest-ninja-static-build \
+  --env BUILD_ARCH="$BUILD_ARCH" \
+  --env BUILD_DIST=ubuntu \
+  --env CFLAGS='-Os -g0 -ffunction-sections -fdata-sections -fvisibility=hidden -fmerge-all-constants' \
+  --env LDFLAGS='-Wl,--gc-sections -Wl,--strip-all' \
+  --env MAKEFLAGS="-j$(nproc)" \
+  vhsgunzo/dwarfs-universal-build-image:latest
